7d52af3de0a999e885005aa2cb5e6c54237a446c
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2024-12-08 18:45:01 +0100

    setup spring boot

---
+++ src/main/java/at/microlab/calendar_app/CalendarAppApplication.java
@@ -0,0 +1,13 @@
+package at.microlab.calendar_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/microlab/calendar_app/CalendarAppApplicationTests.java
@@ -0,0 +1,13 @@
+package at.microlab.calendar_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
