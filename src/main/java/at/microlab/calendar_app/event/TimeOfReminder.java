d6ac334978c7c0b3e09a9eeb00cb38b6743142e1
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2024-12-10 22:45:51 +0100

    dynamic Template

--- src/main/java/at/microlab/calendar_app/event/TimeOfReminder.java
+++ src/main/java/at/microlab/calendar_app/event/TimeOfReminder.java
@@ -1,9 +1,19 @@
 package at.microlab.calendar_app.event;
 
 public enum TimeOfReminder {
-    ONE_DAY,
-    TWO_DAYS,
-    FOUR_DAYS,
-    ONE_WEEK,
-    TWO_WEEKS;
+    ONE_DAY("1 Tag"),
+    TWO_DAYS("2 Tage"),
+    FOUR_DAYS("4 Tage"),
+    ONE_WEEK("1 Woche"),
+    TWO_WEEKS("2 Wochen");
+
+    private final String displayText;
+
+    TimeOfReminder(String displayText) {
+        this.displayText = displayText;
+    }
+
+    public String getDisplayText() {
+        return displayText;
+    }
 }
